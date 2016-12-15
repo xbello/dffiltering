2db500ff2ab4c3b6ba0ecbe95c9f478a846b53bf
Xabi Bello <xbello@gmail.com>
2016-12-15 09:05:37 +0100

    Added the mapping to columns.

--- ff.py
+++ ff.py
@@ -1,7 +1,61 @@
 """Deals with TAB files to load, munge and filter them."""
 import pandas as pd
 
+COLUMN_TYPES = {
+    "numeric":
+    ["Start", "End", "vardb_life", "vardb_gatk", "vardb_tvc", "vardb_1x",
+     "PopFreqMax", "1000G_ALL", "1000G_AFR", "1000G_AMR", "1000G_EAS",
+     "1000G_EUR", "1000G_SAS", "ExAC_ALL", "ExAC_AFR", "ExAC_AMR", "ExAC_EAS",
+     "ExAC_FIN", "ExAC_NFE", "ExAC_OTH", "ExAC_SAS", "ESP6500siv2_ALL",
+     "ESP6500siv2_AA", "ESP6500siv2_EA", "CG46", "SIFT_score",
+     "Polyphen2_HDIV_score", "Polyphen2_HVAR_score", "LRT_score",
+     "MutationTaster_score", "MutationAssessor_score", "FATHMM_score",
+     "PROVEAN_score", "VEST3_score", "CADD_raw", "CADD_phred", "DANN_score",
+     "fathmm-MKL_coding_score", "MetaSVM_score", "MetaLR_score",
+     "integrated_fitCons_score", "integrated_confidence_value",
+     "GERP++_RS", "phyloP7way_vertebrate", "phyloP20way_mammalian",
+     "phastCons7way_vertebrate", "phastCons20way_mammalian",
+     "SiPhy_29way_logOdds", "dbscSNV_ADA_SCORE", "dbscSNV_RF_SCORE", "dann"],
+    "str":
+    ["Chr", "Ref", "Alt", "Func.refGene", "Gene.refGene",
+     "GeneDetail.refGene", "ExonicFunc.refGene", "AAChange.refGene",
+     "Func.ensGene", "Gene.ensGene", "GeneDetail.ensGene",
+     "ExonicFunc.ensGene", "AAChange.ensGene", "phastConsElements46way",
+     "genomicSuperDups", "OMIM_id OMIM_disorder", "GeneReviews_disease",
+     "clinvar_20150330", "avsnp147", "snp144Flagged", "cosmic70", "SIFT_pred",
+     "Polyphen2_HDIV_pred", "Polyphen2_HVAR_pred", "LRT_pred",
+     "MutationTaster_pred", "MutationAssessor_pred", "FATHMM_pred",
+     "PROVEAN_pred", "fathmm-MKL_coding_pred", "MetaSVM_pred", "MetaLR_pred",
+     "Interpro_domain", "Zygosity", "Qual", "Depth", "CHROM", "POS",
+     "ID", "REF", "ALT", "QUAL", "FILTER", "AF", "AO", "DP", "FAO", "FDP",
+     "FR", "FRO", "FSAF", "FSAR", "FSRF", "FSRR", "FWDB", "FXX HRUN",
+     "LEN MLLD", "OALT", "OID", "OMAPALT", "OPOS", "OREF", "QD", "RBI",
+     "REFB", "REVB", "RO", "SAF", "SAR", "SRF", "SRR", "SSEN", "SSEP", "SSSB",
+     "STB", "STBP", "TYPE", "VARB", "GT", "GQ"]}
+
+
+def str_dtypes():
+    dtypes = {}
+    for column in COLUMN_TYPES["str"]:
+        dtypes[column] = str
+
+    return dtypes
+
 
 def load(filepath):
     """Return the filepath loaded as a DataFrame."""
-    return pd.read_table(filepath)
+    dtypes = str_dtypes()
+    try:
+        df = pd.read_table(filepath, dtype=dtypes)
+    except UnicodeDecodeError:
+        df = pd.read_table(filepath, dtype=dtypes, encoding="iso-8859-1")
+
+    # Correct columns with "." to zeroes.
+    df.replace(".", 0, inplace=True)
+    # Correct columns with "1." to ones.
+    df.replace("1.", 1, inplace=True)
+
+    df[COLUMN_TYPES["numeric"]] = df[COLUMN_TYPES["numeric"]].\
+        apply(pd.to_numeric)
+
+    return df
--- test_files/test_ff.py
+++ test_files/test_ff.py
@@ -1,6 +1,7 @@
 """Test the ff module."""
 import os
 import pandas as pd
+import numpy as np
 from unittest import TestCase
 
 from dffiltering import ff
@@ -9,10 +10,27 @@ from dffiltering import ff
 class testDataFrame(TestCase):
     def test_can_load_tab_as_DF(self):
         tab_file = os.path.join(os.path.dirname(__file__),
-                                "SAMPLE.TVC.variants.tab")
+                                "8859.tab")
 
         df = ff.load(tab_file)
 
         self.assertEqual(type(df), pd.DataFrame)
         # The DataFrame has 149 columns and 315 data rows.
-        self.assertEqual(df.shape, (315, 149))
+        self.assertEqual(df.shape, (249, 151))
+
+    def test_load_with_weird_chars(self):
+        tab_file = os.path.join(os.path.dirname(__file__),
+                                "8859.tab")
+
+        df = ff.load(tab_file)
+
+        self.assertEqual(type(df), pd.DataFrame)
+        # The DataFrame has 149 columns and 315 data rows.
+        self.assertEqual(df.shape, (249, 151))
+
+    def test_DF_has_loaded_correctly(self):
+        tab_file = os.path.join(os.path.dirname(__file__), "8859.tab")
+
+        df = ff.load(tab_file)
+
+        self.assertEqual(df["MetaLR_score"].dtype, np.dtype("float64"))
