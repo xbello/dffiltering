b837d53c0f6d87ff3c029d60be294038cf3dbd6f
Xabi Bello <xbello@gmail.com>
2017-01-24 12:59:12 +0100

    Bumped self version. Bumped Pandas version to 0.19.2.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name="dffiltering",
-    version="0.1.4",
+    version="0.1.5",
     description="Command line to filter TSV files.""",
     author_email="xbello@gmail.com",
     packages=["dffiltering.ff", "dffiltering.command"],
     install_requires=[
-        "pandas==0.19.1"],
+        "pandas==0.19.2"],
     entry_points={
         "console_scripts": [
             "dff = dffiltering.command.run:run"]})
