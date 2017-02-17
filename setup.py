8dd22555c374359ce822739917c6bf2a3ba5f04f
Xabi Bello <xbello@localhost.localdomain>
2017-02-17 13:10:45 +0100

    Bumping the version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="dffiltering",
-    version="0.1.6",
+    version="0.1.7",
     description="Command line to filter TSV files.""",
     author_email="xbello@gmail.com",
     packages=["dffiltering.ff", "dffiltering.command"],
